5e55cad62af543d39ff3dc4af66c5eab162418c0
Han Xiao <han.xiao@jina.ai>
2025-08-29 11:36:21 -0700

    rm test files from tracking, keep locally

--- test_cali_loader.py
+++
@@ -1,8 +0,0 @@
-from cali_data_loader_eos import get_cali_stream
-
-stream = get_cali_stream("v8")
-
-for i, batch in enumerate(stream):    
-    print(batch["eos_pos"].shape)
-    if i > 3:
-        break
